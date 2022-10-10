b408a944ba7228304c1a0b39f0afa72d0b700b01
0samvuong <101376900+0samvuong@users.noreply.github.com>
2022-10-10 19:38:31 +1100

    test

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import news_scraper_app
-# import pymongo
+from flask import Flask, render_template, redirect, request
+# from flask_pymongo import PyMongo
+# import news_scraper_app
+# # import pymongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/news_ddb"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/news_ddb"
+# mongo = PyMongo(app)
 
-
+#--------------------------------------
+# Flask Routes (website URL's)
+#--------------------------------------
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/scrape")
-def scrape():
-    scraped_data = news_scraper_app.scraper()
-    mongo.db.news_db.replace_one({}, scraped_data, upsert=True)
-    return redirect("/", code=302)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+#--------------------------------------
+# Flask API CALLS (javascript calling python code)
+#--------------------------------------
+
+# data for number of establishment 
+@app.route("/api/exampleScrape")
+def exampleScrape(testParameter):
+    return testParameter
+
+
+# @app.route("/scrape")
+# def scrape():
+#     scraped_data = news_scraper_app.scraper()
+#     mongo.db.news_db.replace_one({}, scraped_data, upsert=True)
+#     return redirect("/", code=302)
 
 if __name__ == '__main__':
     app.run(debug=True)
