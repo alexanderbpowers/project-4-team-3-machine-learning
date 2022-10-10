90041e642a6c165d6dab7d7d0727dc64bfd9c57f
0samvuong <101376900+0samvuong@users.noreply.github.com>
2022-10-10 18:23:35 +1100

    a

--- static/index.js
+++ static/index.js
@@ -1,3 +1,25 @@
+function random_result() {
+    // for demo purposes
+    var resultsTitle = document.getElementById('resultTitle');
+    var resultsText = document.getElementById('resultText');
+
+    x = Math.random();
+    if(x < 0.5){
+        resultsTitle.innerHTML = ' result less than .5';
+        resultsText.innerHTML = 'RED';
+
+        resultsTitle.style.color = 'red';
+        resultsText.style.color = 'red';
+    }else{
+        resultsTitle.innerHTML = ' result greater than .5';
+        resultsText.innerHTML = 'BLUE';
+
+        resultsTitle.style.color = 'green';
+        resultsText.style.color = 'green';
+    };
+
+}
+
 function analyse_click(p1) {
     // variables
     var analyse_textbox_value = document.getElementById('urlTextbox').value;
@@ -6,5 +28,8 @@ function analyse_click(p1) {
 
     article_heading_text.innerHTML = 'Test heading';
     article_body_text.innerHTML = analyse_textbox_value;
+
+    // calling a random result for testing
+    random_result();
 
   }
